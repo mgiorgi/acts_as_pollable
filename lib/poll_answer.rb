ce5f4eb28455716aef2874126121a5267e03f1f9
manya <manya@lic.(none)>
2009-03-16 11:58:27 -0300

    Removing invalid association for polimorphic PollAnswer

--- lib/poll_answer.rb
+++ lib/poll_answer.rb
@@ -3,8 +3,6 @@ class PollAnswer < ActiveRecord::Base
   belongs_to :pollable, :polymorphic => true
   belongs_to :targetable, :polymorphic => true
   has_one :poll, :through => :option
-  belongs_to :user_target, :class_name => 'User', :foreign_key => 'targetable_id'
-  belongs_to :user_source, :class_name => 'User', :foreign_key => 'pollable_id'
   
   named_scope :votes, lambda { |poll_id, target_type, target_id| { :include=>[:option=>[:poll]], :conditions => ['polls.id = ? AND poll_answers.targetable_type = ? AND poll_answers.targetable_id = ?', poll_id, target_type, target_id]} }
 end
