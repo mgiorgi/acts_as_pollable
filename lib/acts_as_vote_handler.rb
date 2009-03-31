5b260608f532aee2a67c3d617164eb785cb4ae33
manya <manya@willy.(none)>
2009-03-30 22:21:19 -0300

    Fix another bug for pollable_user method

--- lib/acts_as_vote_handler.rb
+++ lib/acts_as_vote_handler.rb
@@ -91,7 +91,7 @@ module Mgm
 
   def pollable_user(pollable_type, pollable_id)
     if pollable_type && pollable_id
-      pollable = pollable_type.to_s.classify.constantize.send(:find, pollable_id)
+      pollable = pollable_type.to_s.classify.constantize.send(:find_by_id, pollable_id)
     else
       current_user
     end
