8afdb7240c6cdfaf98cbc94fae1d48ce5ac1774b
Marcelo Giorgi <marcelo.giorgi.martinez@gmail.com>
2009-04-24 13:35:28 -0300

    Adding new named_scope for filtering using the selected option

--- lib/poll_answer.rb
+++ lib/poll_answer.rb
@@ -4,5 +4,6 @@ class PollAnswer < ActiveRecord::Base
   belongs_to :targetable, :polymorphic => true
   has_one :poll, :through => :option
   
+  named_scope :for_option, lambda { |option_id| { :include=>[:option=>[:poll]], :conditions => ['polls.id = ? AND poll_answers.poll_option_id = ?', option_id]} }
   named_scope :votes, lambda { |poll_id, target_type, target_id| { :include=>[:option=>[:poll]], :conditions => ['polls.id = ? AND poll_answers.targetable_type = ? AND poll_answers.targetable_id = ?', poll_id, target_type, target_id]} }
 end
